e4cd7d64ce2e28c670a37e73aba060bc8168f2f1
ramya <rums0700@gmail.com>
2023-06-17 21:46:52 +0530

    Complete the given assessment with pagination and sorting technique

--- src/App.js
+++ src/App.js
@@ -1,23 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import "./App.css";
+
+const API_URL = 'https://jsonplaceholder.typicode.com/posts';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [error, setError] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 5; // Number of posts to display per page
+  const [sortType, setSortType] = useState('date'); // Initial sort type
+  const [sortOrder, setSortOrder] = useState('asc'); // Initial sort order
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get(API_URL);
+        setPosts(response.data);
+      } catch (error) {
+        setError('Error retrieving posts.');
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+
+  const filteredPosts = posts.filter(
+    (post) =>
+      post.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      post.body.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  let sortedPosts = [...filteredPosts];
+
+  if (sortType === 'date') {
+    sortedPosts.sort((a, b) => {
+      const dateA = new Date(a.date);
+      const dateB = new Date(b.date);
+      if (sortOrder === 'asc') {
+        return dateA - dateB;
+      } else {
+        return dateB - dateA;
+      }
+    });
+  } else if (sortType === 'title') {
+    sortedPosts.sort((a, b) => {
+      if (sortOrder === 'asc') {
+        return a.title.localeCompare(b.title);
+      } else {
+        return b.title.localeCompare(a.title);
+      }
+    });
+  }
+
+  const paginatedPosts = sortedPosts.slice(indexOfFirstPost, indexOfLastPost);
+
+  const totalPages = Math.ceil(filteredPosts.length / postsPerPage);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const handleSortChange = (event) => {
+    setSortType(event.target.value);
+  };
+
+  const handleSortOrderChange = (event) => {
+    setSortOrder(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">POSTS</h1>
+      <div className="sort-section">
+        <label htmlFor="sort-type">Sort by : </label>
+        <select id="sort-type" value={sortType} onChange={handleSortChange}>
+          <option value="date">Date</option>
+          <option value="title">Title</option>
+        </select>
+        <label htmlFor="sort-order">Order : </label>
+        <select id="sort-order" value={sortOrder} onChange={handleSortOrderChange}>
+          <option value="asc">Ascending</option>
+          <option value="desc">Descending</option>
+        </select>
+      </div>
+      <input
+        className="search-input"
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      {error && <p className="error">{error}</p>}
+      <ul className="post-list">
+        {paginatedPosts.map((post) => (
+          <li className="post-item" key={post.id}>
+            <h2 className="post-title">{post.title}</h2>
+            <p className="post-body">{post.body}</p>
+          </li>
+        ))}
+      </ul>
+      <div className="pagination">
+        {Array.from({ length: totalPages }, (_, index) => index + 1).map((pageNumber) => (
+          <button
+            key={pageNumber}
+            className={`pagination-button ${currentPage === pageNumber ? 'active' : ''}`}
+            onClick={() => handlePageChange(pageNumber)}
+          >
+            {pageNumber}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,60 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import axios from 'axios';
 import App from './App';
 
-test('renders learn react link', () => {
+jest.mock('axios');
+
+const mockPosts = [
+  { id: 1, title: 'Post 1', body: 'Body 1' },
+  { id: 2, title: 'Post 2', body: 'Body 2' },
+];
+
+beforeEach(() => {
+  jest.spyOn(axios, 'get').mockResolvedValueOnce({ data: mockPosts });
+});
+
+test('renders posts', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  await waitFor(() => {
+    expect(screen.getByText('Post 1')).toBeInTheDocument();
+    expect(screen.getByText('Post 2')).toBeInTheDocument();
+  });
+});
+
+test('filters posts by title', async () => {
+  render(<App />);
+
+  const searchInput = screen.getByPlaceholderText('Search');
+  userEvent.type(searchInput, '1');
+
+  await waitFor(() => {
+    expect(screen.getByText('Post 1')).toBeInTheDocument();
+    expect(screen.queryByText('Post 2')).not.toBeInTheDocument();
+  });
+});
+
+test('filters posts by body', async () => {
+  render(<App />);
+
+  const searchInput = screen.getByPlaceholderText('Search');
+  userEvent.type(searchInput, 'Body 2');
+
+  await waitFor(() => {
+    expect(screen.getByText('Post 2')).toBeInTheDocument();
+    expect(screen.queryByText('Post 1')).not.toBeInTheDocument();
+  });
+});
+
+test('displays error message', async () => {
+  const errorMessage = 'Error retrieving posts.';
+  jest.spyOn(axios, 'get').mockRejectedValueOnce(new Error(errorMessage));
+
+  render(<App />);
+
+  await waitFor(() => {
+    expect(screen.getByText(errorMessage)).toBeInTheDocument();
+  });
 });
